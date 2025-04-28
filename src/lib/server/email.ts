05dd652a09dd8d0ed93615d26aa9d839704a7f7c
sistemasdsajboy <SistemasBoyaca2025>
2025-04-28 08:54:08 -0500

    Commit inicial

---
+++ src/lib/server/email.ts
@@ -0,0 +1,25 @@
+import { Resend } from 'resend';
+import { env } from '$env/dynamic/private';
+import { dev } from '$app/environment';
+
+const resend = new Resend(env.RESEND_API_KEY);
+
+type SendEmailProps = {
+	subject: string;
+	to: string;
+	html: string;
+};
+
+export async function sendEmail({ subject, to, html }: SendEmailProps) {
+	// Test adress for development only
+	if (dev) {
+		to = 'delivered@resend.dev';
+		console.log({ subject, to, html });
+	}
+
+	const from = 'Consejo Seccional de la Judicatura - Boyacá y Casanare <notificaciones@jhonma.info>';
+	const { data } = await resend.emails.send({ from, to: [to], subject, html });
+
+	if (data?.id) return data.id;
+	return null;
+}
